0f037f2411fc9a5c5dfde202b56c6fd9d802e255
Akshay Patil <asppatil555@gmail.com>
2025-09-18 16:27:24 +0530

    Second program

--- src/com/level2/Two.java
+++ src/com/level2/Two.java
@@ -1,5 +1,20 @@
 package com.level2;
 
+import java.util.Scanner;
+
 public class Two {
+	
+	public void m1(int a, int b, int c) {
+	if(a==c) {
+		System.out.println("it is palindrom");
+	}
+	else {
+		System.out.println("it is not palindrom");
+	}
+	    
+		
+		
+	}
+	
 
 }
